81f39888d818ac0ca85bc4918cd122beafeee50c
aya omar <yotayot29@gmail.com>
2021-04-26 17:39:17 +0300

    Aya 26-4 5.39pm

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\Api\ForgetPasswordController;
 use App\Http\Controllers\Api\UserAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -32,8 +33,15 @@ Route::post('/admin/register', [AdminController::class, 'register']);
 
 Route::post('/admin/login', [AdminController::class, 'login']);
 
+//Route::post('/forgot', [ForgetPasswordController::class, 'forgot']);
 Route::group(['middleware' => ['auth:admins']], function () {
     Route::post('/admin/logout', [AdminController::class, 'logout']);
+    Route::get('/admin/me', [AdminController::class, 'me']);
 //    Route::post('/user/updateProfile', [AdminController::class, 'updateProfile']);
 //    Route::post('/user/editPassword', [AdminController::class, 'editPassword']);
 });
+
+Route::post('/forgot', [ForgetPasswordController::class, 'forgot']);
+Route::post('/password/reset', [ForgetPasswordController::class, 'reset'])->name('password.reset');
+
+
